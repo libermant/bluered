0c8e14f303e716313d932fac3b928cf41db4bfd3
libermant <tsipil@webschool.co.il>
2022-02-02 21:49:50 +0200

    print array

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ function num(){
   for(let i=0;i<32;i++){
     arr.push(i)
   }
+  console.log(arr);
   return arr;  
 }
 /*const number=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]*/
